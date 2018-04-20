5032fa2994774035ecc469a8dfca15ff19a9264c
heyh <577315621@qq.com>
2018-04-20 15:31:21 +0800

    cer

--- Test/src/mytest/MyTest.java
+++ Test/src/mytest/MyTest.java
@@ -2,14 +2,12 @@ package mytest;
 
 public class MyTest {
 
-	
 	public static void main(String[] args) {
 		StringBuffer stringBuffer = new StringBuffer();
 		System.out.println(!stringBuffer.toString().equals(""));
-		
-		//这是冲突内容的显示
-	//sysoadsdas
+
+		// 这是冲突内容的显示
+		// sysoadsdas
 		System.out.println();
-				
 	}
 }
